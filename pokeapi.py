b8c87a45e343513e93f0a32267fb5c66c6de8655
gshklovs <gshklovs@uci.edu>
2022-04-25 16:18:42 -0700

    Full program

---
+++ pokeapi.py
@@ -0,0 +1,151 @@
+# pokeapi.py
+
+
+import json
+import urllib.request
+import urllib.parse
+import urllib.error
+
+POKEAPIBASEURL = 'https://pokeapi.co/api/v2/pokemon/'
+NUMPOKEMONURL = 'https://pokeapi.co/api/v2/pokemon?limit=2000'
+MOVEAPIURL = 'https://pokeapi.co/api/v2/move/'
+
+
+class pokeAPI:
+    #I will later have to make these functions work fot the other pokemon json
+    def __init__(self, pokemon_name, path, is_pokemon=True):
+        print('using api')
+        self.start_url = self._build_url(pokemon_name, is_pokemon)
+        json_object = self._get_json(path)
+
+    def _build_url(self, name, is_pokemon) -> str:
+        """Create a url for the specific pokemon to use for API"""
+        if is_pokemon:
+            return (POKEAPIBASEURL + name)
+        else:
+            return (MOVEAPIURL + name)
+
+    def _get_json(self, path) -> json:
+        """Get the json object for a pokemon from the API (basically a list of list that has all the data we need)"""
+        response = None
+        try:
+            #The 'Code Club' header is required or a 403 error occurs
+            request = urllib.request.Request(self.start_url, headers={'User-Agent': 'Code Club'})
+            response = urllib.request.urlopen(request)
+            self.json_object = json.loads(response.read().decode(encoding='UTF-8'))
+        except urllib.error.HTTPError:
+            print('HTTP Error 404: Not Found')
+            print(f'Tried to access {self.start_url}')
+        finally:
+            if response:
+                response.close()
+        self._make_file(path, self.json_object)
+        return self.json_object
+    
+    def _make_file(self, path, contents) -> None:
+        """Create a file at the objects path with the json object as contents"""
+        with open(path, 'w') as file:
+            file.write(json.dumps(contents))
+
+    def get_stats(self) -> list[str]:
+        """Return a list of stats summarizing the stat names and their values"""
+        stats_list = []
+        for stat in self.json_object['stats']:
+            stats_str = stat['stat']['name']
+            stats_str += ': ' + str(stat['base_stat'])
+            stats_list.append(stats_str)
+        return stats_list
+
+    def get_types(self) -> str:
+        """Return a string naming the type of the pokemon"""
+        json_types = self.json_object['types']
+        if len(json_types) > 1 :
+            type_list = []
+            for i in json_types:
+                type_list.append(i['type']['name'])
+            type_output = '/'.join(type_list)
+        else:
+            type_output = json_types[0]['type']['name']
+        return type_output
+
+    def get_moves(self) -> list:
+        """Return a list of moves that a pokemon can do"""
+        return [move['move']['name'] for move in self.json_object['moves']]
+
+    def get_move_info(self) -> tuple:
+        """Get a tuple of damage class, accuracy, damage, and type"""
+        accuracy = self.json_object['accuracy'] if self.json_object['accuracy'] else 100
+        damage_class = self.json_object['damage_class']['name']
+        power = self.json_object['power'] if self.json_object['power'] else 0
+        move_type = self.json_object['type']['name']
+        return accuracy, damage_class, power, move_type
+
+class pokeFile:
+    def __init__(self, pokemon_name, path):
+        self.name = pokemon_name
+        self.path = path
+        json_object = self._get_json(path)
+        
+    def _get_json(self, path) -> json:
+        """Parses the json form a pokemon's file"""
+        with open(path, 'r') as file:
+            json_text = file.read()
+            self.json_object = json.loads(json_text)
+        return self.json_object
+    
+    def get_stats(self) -> dict:
+        """Return a list of stats summarizing the stat nmaes and their values"""
+        stats_dict = {}
+        for stat in self.json_object['stats']:
+            stats_dict[stat['stat']['name']] = stat['base_stat']
+        return stats_dict
+
+    def get_types(self) -> str:
+        """Return a string naming the type of the pokemon"""
+        json_types = self.json_object['types']
+        if len(json_types) > 1 :
+            type_list = []
+            for i in json_types:
+                type_list.append(i['type']['name'])
+            type_output = '/'.join(type_list)
+        else:
+            type_output = json_types[0]['type']['name']
+        return type_output
+
+    def get_moves(self) -> list:
+        """Return a list of moves that a pokemon can do"""
+        return [move['move']['name'] for move in self.json_object['moves']]
+
+    def get_move_info(self) -> tuple:
+        """Get a tuple of damage class, accuracy, damage, and type"""
+        accuracy = self.json_object['accuracy'] if self.json_object['accuracy'] else 100
+        damage_class = self.json_object['damage_class']['name']
+        power = self.json_object['power'] if self.json_object['power'] else 0
+        move_type = self.json_object['type']['name']
+        return accuracy, damage_class, power, move_type
+
+
+class NumPokemonHandler:
+    def __init__(self):
+        self.stats_dict = {}
+        self.contents_url = NUMPOKEMONURL
+        self.table_of_contents = self._get_json(self.contents_url)
+
+
+    def _get_json(self, url) -> json:
+        """Get the json object from a given URL"""
+        response = None
+        try:
+            # The 'Code Club' header is required or a 403 error occurs
+            request = urllib.request.Request(url, headers={'User-Agent': 'Code Club'})
+            response = urllib.request.urlopen(request)
+            json_object = json.loads(response.read().decode(encoding='UTF-8'))
+        finally:
+            if response:
+                response.close()
+        return json_object
+
+    def get_contents(self) -> list:
+        """Return a list of the pokemon names that need to be referenced"""
+        return [pokemon['name'] for pokemon in self.table_of_contents['results']]
+
---
+++ pokemon.py
@@ -0,0 +1,71 @@
+# pokemon.py
+# the first time this program is run will take longer than all the rest
+import pokeapi as api
+from pathlib import Path
+from calc import calculate_viability_index
+
+
+def run(pokemon_name, list_len=10):
+    """Get the top counters of a given pokemon"""
+    #return ['sceptile-mega', 'exeggutor-alola', 'zekrom', 'ampharos-mega', 'eternatus-eternamax']
+    #pokemon_name = get_pokemon_input()
+    #pokemon_name = 'charmander'
+    pokeapi_handler = get_handler(pokemon_name)
+    my_stats_list = pokeapi_handler.get_stats()
+    my_type = pokeapi_handler.get_types()
+    list_of_names = get_pokemon_list()
+    pokemon_to_viability_index = get_viability_list(list_of_names, my_type)
+    return pokemon_to_viability_index[:list_len]
+
+
+def get_pokemon_list() -> list:
+    """Get a list of all pokemon names that I plan on parsing through"""
+    pokemon_contents_handler = api.NumPokemonHandler()
+    return pokemon_contents_handler.get_contents()
+
+
+def get_viability_list(list_of_names: list, my_types: str) -> list:
+    """Return a dictionary of pokemon to their viability index"""
+    name_to_viability = {}
+    for name in list_of_names:
+        handler = get_handler(name)
+        moves = get_moves_objects(handler)
+        name_to_viability[name] = calculate_viability_index(handler, my_types, moves)
+    return sorted(name_to_viability, key=lambda x: name_to_viability[x], reverse=True)
+
+
+def get_pokemon_input() -> str:
+    pokemon = input('Enter pokemon name or number\n>>> ')
+    if pokemon.isdigit():
+        pass
+    else:
+        pokemon = pokemon.lower()
+    return pokemon
+
+
+def get_handler(pokemon_name: str, path_name='cache', is_pokemon=True) -> 'pokemon_handler':
+    """Get the correct type of handler depending on if a file already exists"""
+    # check if a folder exists in the directory, if it doesn't, make a folder
+    dir_path = Path(path_name)
+    if not dir_path.exists():
+        dir_path.mkdir(exist_ok = True)
+
+    pokemon_path = dir_path / pokemon_name
+    if pokemon_path.exists():
+        return api.pokeFile(pokemon_name, pokemon_path)
+    else:
+        return api.pokeAPI(pokemon_name, pokemon_path, is_pokemon=is_pokemon)
+
+
+def get_moves_objects(handler) -> list:
+    """Get a list of all the moves handler objects"""
+    moves_list = handler.get_moves()
+    move_objects = []
+    for move_name in moves_list:
+        move_handler = get_handler(move_name, path_name='move_cache', is_pokemon=False)
+        move_objects.append(move_handler)
+    return move_objects
+
+
+if __name__ == "__main__":
+    run()
